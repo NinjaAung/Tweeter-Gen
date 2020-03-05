e83da82186984e3540347befb9fe6fd27d3453a6
ninjaaung <ninja.aung02@gmail.com>
2020-03-05 09:17:07 -0800

    Improved Linked list | Completed Hashtable

--- hashtable.py
+++ hashtable.py
@@ -1,7 +1,5 @@
 #!python
-
 from linkedlist import LinkedList
-
 
 class HashTable(object):
 
@@ -9,6 +7,7 @@ class HashTable(object):
         """Initialize this hash table with the given initial size."""
         # Create a new list (used as fixed-size array) of empty linked lists
         self.buckets = [LinkedList() for _ in range(init_size)]
+        self.count = 0
 
     def __str__(self):
         """Return a formatted string representation of this hash table."""
@@ -19,15 +18,34 @@ class HashTable(object):
         """Return a string representation of this hash table."""
         return 'HashTable({!r})'.format(self.items())
 
+    # python __iter__ method to make the hashtable iterable
+    def __iter__(self):
+        for bucket in self.buckets:
+            for item in bucket.items():
+                yield item
+
     def _bucket_index(self, key):
         """Return the bucket index where the given key would be stored."""
-        # Calculate the given key's hash code and transform into bucket index
         return hash(key) % len(self.buckets)
+    
+    def _get_item(self, key):
+        '''helper function returns the item and bucket, inspired from Ben Lafferty'''
+        if self.contains(key):
+            index = self._bucket_index(key)
+            bucket = self.buckets[index]
+            
+            item = bucket.find(lambda item: item[0] == key)
+
+            return item, bucket
+        else:
+            raise KeyError('Key not found: {}'.format(key))
+
+
 
     def keys(self):
         """Return a list of all keys in this hash table.
-        TODO: Running time: O(???) Why and under what conditions?"""
-        # Collect all keys in each bucket
+        Running time: O(n) Why and under what conditions?
+            checks a list of n items"""
         all_keys = []
         for bucket in self.buckets:
             for key, value in bucket.items():
@@ -36,13 +54,19 @@ class HashTable(object):
 
     def values(self):
         """Return a list of all values in this hash table.
-        TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Loop through all buckets
-        # TODO: Collect all values in each bucket
+        Running time: O(n) Why and under what conditions?
+            checks a list of n items"""
+        
+        all_values = []
+        for bucket in self.buckets:
+            for key, value in bucket.items():
+                all_values.append(value)
+        return all_values
 
     def items(self):
         """Return a list of all items (key-value pairs) in this hash table.
-        TODO: Running time: O(???) Why and under what conditions?"""
+        Running time: O(n) Why and under what conditions?
+                there is a linear amount of items in the hashtable"""
         # Collect all pairs of key-value entries in each bucket
         all_items = []
         for bucket in self.buckets:
@@ -51,41 +75,55 @@ class HashTable(object):
 
     def length(self):
         """Return the number of key-value entries by traversing its buckets.
-        TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Loop through all buckets
-        # TODO: Count number of key-value entries in each bucket
+        Running time: O(1) Why and under what conditions?
+                constant number of operations"""
+        
+        return self.count
+
 
     def contains(self, key):
         """Return True if this hash table contains the given key, or False.
-        TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Find bucket where given key belongs
-        # TODO: Check if key-value entry exists in bucket
+        Running time: O(???) Why and under what conditions?"""
+        
+        if key in self.keys():
+            return True
+        else:
+            return False
+        
 
     def get(self, key):
         """Return the value associated with the given key, or raise KeyError.
-        TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Find bucket where given key belongs
-        # TODO: Check if key-value entry exists in bucket
-        # TODO: If found, return value associated with given key
-        # TODO: Otherwise, raise error to tell user get failed
-        # Hint: raise KeyError('Key not found: {}'.format(key))
-
+        Running time: O(n) Why and under what conditions?
+                Checks a list of n items, linear time"""
+        
+        
+        item, bucket = self._get_item(key)
+        return item.data[1]
+        
     def set(self, key, value):
         """Insert or update the given key with its associated value.
-        TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Find bucket where given key belongs
-        # TODO: Check if key-value entry exists in bucket
-        # TODO: If found, update value associated with given key
-        # TODO: Otherwise, insert given key-value entry into bucket
+        Running time: O(1)/O(n) Why and under what conditions?
+            If the key doesnt  exist then adds directly to the end. if it does its linear in checking the list"""
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        
+        if self.contains(key):
+            item = bucket.find(lambda item: item[0] == key)
+            bucket.replace(item.data, (key, value))
+        else:
+            bucket.append((key, value))
+            self.count += 1
+
+
 
     def delete(self, key):
         """Delete the given key from this hash table, or raise KeyError.
-        TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Find bucket where given key belongs
-        # TODO: Check if key-value entry exists in bucket
-        # TODO: If found, delete entry associated with given key
-        # TODO: Otherwise, raise error to tell user delete failed
-        # Hint: raise KeyError('Key not found: {}'.format(key))
+        Running time: O(n) Why and under what conditions?
+                If there are a lot of items in the list, there is a linear amount of operations"""
+        
+        item, bucket = self._get_item(key)
+        bucket.delete(item.data)
+        self.count -= 1
 
 
 def test_hash_table():
@@ -108,8 +146,13 @@ def test_hash_table():
 
     # Enable this after implementing delete method
     delete_implemented = False
+
     if delete_implemented:
         print('\nTesting delete:')
+        print(ht.keys())
+        print(ht.length())
+        print()
+
         for key in ['I', 'V', 'X']:
             print('delete({!r})'.format(key))
             ht.delete(key)
--- linkedlist.py
+++ linkedlist.py
@@ -4,10 +4,11 @@ class Node(object):
         """Initialize this node with the given data."""
         self.data = data
         self.next = None
+        self.previous = None
 
     def __repr__(self):
         """Return a string representation of this node."""
-        return f'Node({repr(self.data)})'
+        return 'Node({!r})'.format(self.data)
 
 
 class LinkedList(object):
@@ -16,10 +17,12 @@ class LinkedList(object):
         """Initialize this linked list and append the given items, if any."""
         self.head = None  # First node
         self.tail = None  # Last node
+        self.count = 0 # length of List
         # Append given items
         if items is not None:
             for item in items:
                 self.append(item)
+                
 
     def __str__(self):
         """Return a formatted string representation of this linked list."""
@@ -29,6 +32,20 @@ class LinkedList(object):
     def __repr__(self):
         """Return a string representation of this linked list."""
         return 'LinkedList({!r})'.format(self.items())
+
+    
+    def __iter__(self):
+        """allows for iteration through the linked list, uses the python __iter__() method to allow this."""
+        # set the initial node to the head
+        node = self.head
+
+        # iterate in a range of the length of the linked list
+        for _ in range(self.count):
+            #  yield the node data. 
+            # yield works similarily to return, however it allows the function to continue running
+            yield node.data
+            # move on to the next node
+            node = node.next
 
     def items(self):
         """Return a list (dynamic array) of all items in this linked list.
@@ -50,37 +67,30 @@ class LinkedList(object):
         return self.head is None
 
     def length(self):
-        """Return the length of this linked list by traversing its nodes.
-        TODO: Running time: O(1) because it is not dependent on the amount of elemnts"""
-        if self.is_empty:
-            size = 0
-            node = self.head
-            while node is not None:
-                size += 1
-                node = node.next
-            return size
-            
-
-
+        """Return the length of this linked list by traversing its nodes."""
+        return self.count
 
     def append(self, item):
         """Insert the given item at the tail of this linked list.
-        TODO: Running time: O(1) because it is not dependent on the amount of elemnts """
-        self.tail = Node(item)
-        node = self.head
-        if node is None:
-            self.head = Node(item)
+        Running time: O(1) 
+        because it only has to complete a constant number of steps."""
+        
+        node = Node(item)
+        if self.is_empty():
+            self.head = node
+            self.tail = node
+            self.count += 1
         else:
-            while node.next != self.tail:
-                if node.next is None:
-                    node.next = Node(item)
-                    break
-                else:
-                    node = node.next
+            self.tail.next = node
+            node.previous = self.tail
+            self.tail = node
+            self.count += 1
+
 
     def prepend(self, item):
         """Insert the given item at the head of this linked list.
-        Running time: O(1) because it is not dependent on the amount of elemnts 
+        Running time: O(1) 
+        because it only has to complete a constant number of steps. 
         """
         
         node = Node(item)
@@ -94,50 +104,64 @@ class LinkedList(object):
             self.head.previous = node 
             self.head = node
             self.count += 1
-    
-    def replace(self, find_item, new_item):
-        """finds and replaces a node with the given arguments."""
-        node = self.find(lambda item: item == find_item)
-        node.data = new_item
-        return node
-        
+
+
 
     def find(self, quality):
         """Return an item from this linked list satisfying the given quality.
-        Best case running time: O(1) if there was =< 3 nodes
-        Worst case running time: O(n) if we were to loop through each element and > 3  """
+        Best case running time: O(1) Why and under what conditions?
+            if finding the head
+        Worst case running time: O(n) Why and under what conditions?
+            if there are a lot of items in the list"""
+       
         node = self.head
 
-            while node is not None:
-                if quality(node.data) == False:
-                    node = node.next
-                elif node.data is not None:
-                    return node.data
+        while node != None:
+            if quality(node.data) == False:
+                node = node.next
+            elif node.data != None:
+                # return node.data
+                return node
+
+        return None
+
+    def replace(self, find_item, new_item):
+        """finds and replaces a node with the given arguments. 
+        Best case running time: O(1) Why and under what conditions?
+            if finding the head
+        Worst case running time: O(n) Why and under what conditions?
+            if there are a lot of items in the list"""
+        node = self.find(lambda item: item == find_item)
+        node.data = new_item
+        return node
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError.
-        Best case running time: O(1) if there was =< 3 nodes
-        Worst case running time: O(n) depending on the size of the linked list. if we were to loop through each element and > 3"""
-        node = self.head
-        prev_node = None
-        while node:
-            if node.data == item:
-                if prev_node == None:
-                    self.head = node.next
-                    if node.next == None:
-                        self.tail = None
-                elif node.next == None:
-                    prev_node.next = None
-                    self.tail = prev_node
-                else:
-                    prev_node.next = node.next
-                return
-            else:
-                prev_node = node
-                node = node.next
-        raise ValueError(f'Item not found: {item}')
-
-
+        Best case running time: O(1) Why and under what conditions?
+            If deleting the head
+        Worst case running time: O(n) Why and under what conditions?
+            depending on the size of the linked list. if there are a lot of items in the list"""
+        
+        if item in self.items():
+            node = self.find(lambda data: data == item)
+            if self.head != node and self.tail != node:
+                node.previous.next = node.next
+                node.next.previous = node.previous
+                self.count -= 1
+            elif self.head == node and self.tail == node:
+                self.head = None
+                self.tail = None
+                self.count = 0
+            elif self.head == node and self.tail != node:
+                self.head = self.head.next
+                self.head.previous = None
+                self.count -= 1
+            elif self.head != node and self.tail == node:
+                self.tail = self.tail.previous
+                self.tail.next = None
+                self.count -= 1
+        else:
+            raise ValueError('Item not found: {}'.format(item))
 
 def test_linked_list():
     ll = LinkedList()
@@ -176,3 +200,8 @@ def test_linked_list():
 if __name__ == '__main__':
     test_linked_list()
 
+    
+
+
+if __name__ == '__main__':
+    test_linked_list()

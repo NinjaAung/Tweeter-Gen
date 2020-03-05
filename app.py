c1cb391ba97386f03e85d2e0e51af20c9f61d0dc
ninjaaung <ninja.aung02@gmail.com>
2020-03-05 09:07:20 -0800

    Flask App and Markov chain File Intpret

--- app.py
+++ app.py
@@ -1,17 +1,72 @@
 from dictogram import Dictogram
+from markov import MarkovChain
 
 from flask import Flask
 app = Flask(__name__)
 test = "one fish two fish red fish blue fish"
 
 @app.route('/')
+def send_to():
+    return '''
+<html>
+    <style>
+
+    </style>
+        <head>
+            <title>Route to Code</title>
+        </head>
+   <body>
+   
+      <button onclick="window.location.href = '/histogram';">Histogram Example</button>
+      <button onclick="window.location.href = '/markov';">Markov Chain example</button>
+   </body>
+</html>
+'''
+
+
+@app.route('/histogram')
 def randomized_word():
     histogram = Dictogram(test.split())
     random_words = ""
     n = "\n"
     for _ in range(0, histogram.tokens):
         random_words += f' {histogram.sample()}'
-    return f'Sample text: {test}|Histogram of Words: {histogram}|Random words: {random_words}'
+    return f'''
+    <html>
+        <head>
+            <title> Histogram </title>
+        </head>
+    <body>
+    Sample text: {test}
+    <br>
+    Histogram of Words: {histogram}
+    <br>
+    Random words: {random_words}
+    <br>
+    <button onclick="window.location.href = '/markov';">Markov Chain example</button>
+    <button onclick="window.location.href = '/';">Home</button>
+    </body>
+    </html>
+    '''
+
+@app.route('/markov')
+def randomized_markov():
+    markov_chain = MarkovChain("book.txt")
+    return f'''
+    <html>
+        <head>
+            <title> Markov Chain </title>
+        </head>
+    <body>
+    Generated words: {markov_chain.walk(10)}
+    </body>
+    <br>
+    <button onclick="window.location.href = '/histogram';">Histogram</button>
+    <button onclick="window.location.href = '/';">Home</button>
+    <\html>
+    '''
+
+    
 
 
 if __name__ == '__main__':
--- markov.py
+++ markov.py
@@ -1,17 +1,18 @@
 from dictogram import Dictogram
 from random import choice
+from os import path
+import os.path
+import re
 
 class MarkovChain:
 
     def __init__(self, word_list):
+        try:
+            self.markov_chain = self.build_markov(self.clean_file(word_list))
+        except TypeError:
+            self.markov_chain = self.build_markov(word_list)
 
-
-        #The Markov chain will be a dictionary of dictionaries
-        #Example: for "one fish two fish red fish blue fish"
-        #{"one": {fish:1}, "fish": {"two":1, "red":1, "blue":1}, "two": {"fish":1}, "red": {"fish":1}, "blue": {"fish:1"}}
-        self.markov_chain = self.build_markov(word_list)
         self.first_word = list(self.markov_chain.keys())[0]
-
 
     def build_markov(self, word_list):
         markov_chain = {}
@@ -48,8 +49,8 @@ class MarkovChain:
         for word, histogram in self.markov_chain.items():
             print(word, histogram.dictionary_histogram)
 
-
-
-markov_chain = MarkovChain(["one", "fish", "two", "fish", "red", "fish", "blue", "fish"])
-markov_chain.print_chain()
-print(markov_chain.walk(10))
+    def clean_file(self, file):
+        with open(file, 'r') as file:
+            open_file = file.read().lower()
+            words = re.sub(r'[^a-zA-Z\s]', '', open_file)
+        return words.split()
